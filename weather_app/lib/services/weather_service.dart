5d243247c7894520626bb649632102870ecbda67
Jedrick28 <jedricktenedero49@gmail.com>
2026-01-19 08:39:11 +0800

    Remove hardcoded API key from WeatherService
    Removed the hardcoded API key for security reasons.

--- weather_app/lib/services/weather_service.dart
+++ weather_app/lib/services/weather_service.dart
@@ -4,7 +4,7 @@ import '../models/weather.dart';
 
 class WeatherService {
   // Static Constants
-  static const String apiKey = '3eb3bb2c62b93b325690cd91729784cb';
+  static const String apiKey = '';
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   static Future<Weather> getWeather(String cityName) async {
